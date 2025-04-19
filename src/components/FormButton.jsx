e104d2d6bd67f0ecc8d300606012174162b86a25
Amanar Marouane <marouane.amanar07@gmail.com>
2025-04-19 19:09:27 +0100

    Fixing requests multing in Login/signup/logout

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { useState } from "react";
 
 const FormButton = ({ title }) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleClick = () => {
+        setTimeout(() => {
+            setIsLoading(true);
+    
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 800);
+        }, 20);
+    };
+    
+
+
     return (
-        <button type="submit" className="px-6 py-4 bg-black text-white rounded-lg w-full cursor-pointer">
-            {title}
+        <button
+            type="submit"
+            onClick={() => { handleClick() }}
+            disabled={isLoading}
+            className={`px-6 py-4 bg-black text-white rounded-lg w-full cursor-pointer relative transition-all duration-300 flex items-center justify-center ${isLoading ? "opacity-70 cursor-not-allowed" : ""
+                }`}
+        >
+            {isLoading ? (
+                <div className="w-6 h-6 border-4 border-white border-t-transparent rounded-full animate-spin" />
+            ) : (
+                <span className="transition-opacity duration-300">{title}</span>
+            )}
         </button>
     );
 };
--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,18 +1,36 @@
-import React, { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from '../context/UserContext';
 
 const LogoutButton = () => {
     const { logout } = useContext(Context);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleLogout = (e) => {
         e.preventDefault();
+        setTimeout(() => {
+            setIsLoading(true);
+
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 800);
+        }, 20);
         logout();
     }
 
     return (
         <form onSubmit={handleLogout}>
-            <button type="submit" className="px-5 py-3 border-1 text-black rounded-lg cursor-pointer">
-                LogOut
+            <button
+                type="submit"
+                onSubmit={handleLogout}
+                disabled={isLoading}
+                className={`px-5 py-3 border-1 text-black rounded-lg cursor-pointer relative transition-all duration-300 flex items-center justify-center ${isLoading ? "opacity-70 cursor-not-allowed" : ""
+                    }`}
+            >
+                {isLoading ? (
+                    <div className="w-6 h-6 border-4 border-black border-t-transparent rounded-full animate-spin" />
+                ) : (
+                    <span className="transition-opacity duration-300">Logout</span>
+                )}
             </button>
         </form>
     );
